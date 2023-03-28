d63e970af48515fec8e20ade279f2611f5d2eb92
Aleksey Vasilev <vasiliev.an@edu.spbstu.ru>
2023-03-28 12:46:58 +0300

    #6 server routing done

--- src/main/kotlin/aleksey/vasiliev/Application.kt
+++ src/main/kotlin/aleksey/vasiliev/Application.kt
@@ -1,15 +1,82 @@
 package aleksey.vasiliev
 
+import aleksey.vasiliev.ServerParameters.dbName
+import aleksey.vasiliev.ServerParameters.isFirst
+import aleksey.vasiliev.ServerParameters.nodesToSend
+import aleksey.vasiliev.ServerParameters.port
+import aleksey.vasiliev.plugins.configureRouting
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import aleksey.vasiliev.plugins.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.Option
+import org.apache.commons.cli.Options
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+fun main(args: Array<String>) {
+    cmdArgsParser(args)
+    embeddedServer(Netty, port = port, module = Application::module).start(wait = true)
+}
+
+fun cmdArgsParser(args: Array<String>) {
+    val summaryOptions = Options()
+    val nodeOption = Option(
+        "n",
+        "nodes",
+        true,
+        "Specify the blockchain nodes in format: http://127.0.0.1:8080,http://127.0.0.1:8081"
+    )
+    nodeOption.args = 1
+    nodeOption.setOptionalArg(false)
+    val dbOption = Option(
+        "d",
+        "database",
+        true,
+        "Specify where to write nodes to check them if you want"
+    )
+    dbOption.args = 1
+    dbOption.setOptionalArg(true)
+    val portOption = Option("p", "port", true, "Specify the port to open the server")
+    portOption.args = 1
+    portOption.setOptionalArg(false)
+    portOption.argName = "port "
+    val firstOption = Option("f", "first", false, "If set, the server will generate the first block")
+    firstOption.setOptionalArg(true)
+    firstOption.argName = "first block "
+    summaryOptions.addOption(portOption)
+    summaryOptions.addOption(firstOption)
+    summaryOptions.addOption(nodeOption)
+    summaryOptions.addOption(dbOption)
+    val defaultParser = DefaultParser()
+    val parsedCmdLine = defaultParser.parse(summaryOptions, args)
+    if (parsedCmdLine.hasOption("p")) {
+        val parsedPort = parsedCmdLine.getOptionValues("p")[0].toIntOrNull()
+            ?: throw IllegalArgumentException("Port should be an Int number!")
+        port = parsedPort
+    } else {
+        throw IllegalArgumentException("Please, specify the server port!")
+    }
+    if (parsedCmdLine.hasOption("n")) {
+        val nodes = parsedCmdLine.getOptionValues("n")[0]
+        val splitNodes = nodes.split(",")
+        if (splitNodes.any { !it.matches(Regex("https?://((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b){4}:\\d{1,5}")) }) {
+            throw IllegalArgumentException("Incorrect node address!")
+        }
+        nodesToSend = splitNodes.toSet()
+    } else {
+        throw IllegalArgumentException("Please, specify the nodes!")
+    }
+    if (parsedCmdLine.hasOption("f")) isFirst.set(true) else isFirst.set(false)
+    if (parsedCmdLine.hasOption("d")) dbName = parsedCmdLine.getOptionValues("d")[0]
 }
 
 fun Application.module() {
-    configureRouting()
+    val blockProducing = BlockProducing()
+    launch(Dispatchers.IO) {
+        configureRouting(blockProducing)
+    }
+    launch(Dispatchers.IO) {
+        blockProducing.produceBlocks()
+    }
 }
--- src/main/kotlin/aleksey/vasiliev/plugins/Routing.kt
+++ src/main/kotlin/aleksey/vasiliev/plugins/Routing.kt
@@ -1,13 +1,67 @@
 package aleksey.vasiliev.plugins
 
-import io.ktor.server.routing.*
-import io.ktor.server.response.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import aleksey.vasiliev.Block
+import aleksey.vasiliev.Block.Companion.getBlockHash
+import aleksey.vasiliev.Block.Companion.isBlockValid
+import aleksey.vasiliev.BlockProducing
+import aleksey.vasiliev.ServerParameters.isFirst
+import aleksey.vasiliev.ServerParameters.port
+import java.math.BigInteger
+import java.util.logging.Logger
 
-fun Application.configureRouting() {
+suspend fun Application.configureRouting(blockProducing: BlockProducing) {
+    Logger.getGlobal().info("Server started at $port port")
+    if (isFirst.get()) {
+        val producedBlock = blockProducing.produceFirstBlock()
+        Logger.getGlobal().info("Produced block: $producedBlock")
+        blockProducing.currentBlock.set(producedBlock)
+        blockProducing.sendBlockToNodes(producedBlock)
+        blockProducing.dbHelper.writeToDB(producedBlock)
+    }
+    install(ContentNegotiation) {
+        json()
+    }
     routing {
+        post("/update") {
+            val frozenBlock = blockProducing.currentBlock.get()
+            val possibleBlock = call.receive<Block>()
+            val hash = possibleBlock.getBlockHash()
+            if (frozenBlock != null) {
+                if (hash.isBlockValid() &&
+                    frozenBlock.hash == possibleBlock.prev_hash &&
+                    BigInteger(possibleBlock.index).subtract(BigInteger(frozenBlock.index)) == BigInteger.ONE
+                ) {
+                    val isSet = blockProducing.currentBlock.compareAndSet(frozenBlock, possibleBlock)
+                    if (isSet) {
+                        Logger.getGlobal().info("Updated block: $possibleBlock")
+                        blockProducing.sendBlockToNodes(possibleBlock)
+                        blockProducing.dbHelper.writeToDB(possibleBlock)
+                    } else {
+                        val block = blockProducing.findTheFreshestBlock()
+                        Logger.getGlobal().info("Updated block: $block")
+                        blockProducing.currentBlock.set(block)
+                        blockProducing.sendBlockToNodes(possibleBlock)
+                        blockProducing.dbHelper.writeToDB(block)
+                    }
+                }
+            } else {
+                Logger.getGlobal().info("Updated block: $possibleBlock")
+                blockProducing.currentBlock.set(possibleBlock)
+                blockProducing.sendBlockToNodes(possibleBlock)
+                blockProducing.dbHelper.writeToDB(possibleBlock)
+            }
+        }
         get("/") {
-            call.respondText("Hello World!")
+            if (blockProducing.currentBlock.get() != null) {
+                Logger.getGlobal().info("Send block: ${blockProducing.currentBlock.get()}")
+                call.respond(blockProducing.currentBlock.get())
+            }
         }
     }
 }
